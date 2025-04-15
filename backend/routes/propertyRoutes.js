b7ca86668e67d29e40d41182843fcd437dec967c
pandya-kunj <kunjpandya@fxis.ai>
2025-04-15 11:50:33 +0530

    Add changes

--- backend/routes/propertyRoutes.js
+++ backend/routes/propertyRoutes.js
@@ -1,15 +1,13 @@
 import express from "express";
-import {
-  createProperty,
-  getAllProperties,
-  getPropertyById,
-  updateProperty,
-  deleteProperty,
-} from "../controllers/propertyController.js";
+import { createProperty, getAllProperties,  getPropertyById,  updateProperty, deleteProperty } from "../controllers/propertyController.js";
+
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+
+
+router.post("/create", isAuthenticated, createProperty);
 
 router.get("/all", getAllProperties);
 
@@ -17,13 +15,13 @@ router.get("/all", getAllProperties);
 router.get("/:id", getPropertyById);
 
 
-router.post("/create", isAuthenticated, createProperty);
-
-
-
 router.put("/update/:id", isAuthenticated, updateProperty);
 
 
 router.delete("/delete/:id", isAuthenticated, deleteProperty);
+
+
+
+
 
 export default router;
